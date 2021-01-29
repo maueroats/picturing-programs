7e63bc9a74480c9675d4c71ef2b4faa2a13a3292
Mauer-Oats <maueroats@gmail.com>
2021-01-29 12:13:47 -0600

    use normalize-color-string when converting names to colors

--- picturing-programs/private/map-image.rkt
+++ picturing-programs/private/map-image.rkt
@@ -27,7 +27,7 @@
          racket/class
          2htdp/image
          (only-in htdp/error natural?)
-         (only-in mrlib/image-core render-image))
+         (only-in mrlib/image-core render-image normalize-color-string))
 ;(require picturing-programs/book-pictures)
 
 ;(require mrlib/image-core)
@@ -107,9 +107,10 @@
     (error 'name->color 
            (format "Expected a string or symbol, but received ~v" name)))
   (let [[result (send the-color-database find-color 
-                      (if (string? name)
-                          name
-                          (symbol->string name)))]]
+                      (normalize-color-string
+                       (if (string? name)
+                           name
+                           (symbol->string name))))]]
     (if result
         (color%->color result)
         #f)))
@@ -117,6 +118,8 @@
 (module+ test
          (check-expect (name->color "red") (make-color 255 0 0 255))
          (check-expect (name->color "plaid") #f)
+         ;; "grey" is normalized to "gray" by normalize-color-string
+         (check-expect (name->color "grey") (make-color 190 190 190 255))
          (check-error (name->color 7 "name->color: Expected a string or symbol, but received 7"))
          )
 
